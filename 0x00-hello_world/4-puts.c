492a15e1acc20be188804a0548002335c6e9fe4c
Uye Princess Arit <uyearit23@gmail.com>
2022-09-08 16:00:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * Main - Entry point
+ * * main - prints the size of various types
  *
- * Return: Always 0 (Success)
+ * * Return: 0 if exited properly, non-zero otherwise
+ *
+ *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
