cc3ea398ace7b1b24a6427ca053e32975fc3ed08
Uye Princess Arit <uyearit23@gmail.com>
2022-09-13 20:55:51 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,3 +32,14 @@ void print_times_table(int n)
 }
 	else
 {
+	_putchar (((op / 100) % 10) + 48);
+	_putchar (((op / 10) % 10) + 48);
+	_putchar ((op % 10) + 48);
+}
+}
+{
+	_putchar ('\n');
+}
+}
+}
+}
