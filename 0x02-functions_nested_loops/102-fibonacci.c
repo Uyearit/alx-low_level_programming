8058f5437407bb9a9b427736e1a56e99dd16faa5
Uye Princess Arit <uyearit23@gmail.com>
2022-09-13 21:03:52 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,33 +7,33 @@
  */
 int main(void)
 {
-  /* starting numbers */
-  long int x = 1;
-  long int y = 2;
+	/* starting numbers */
+	long int x = 1;
+	long int y = 2;
 
-  /* counts how many numbers have been computed*/
-  int counter = 2;
+	/* counts how many numbers have been computed*/
+	int counter = 2;
 
-  /* temporary variable to hold x */
-  long int tmp;
+	/* temporary variable to hold x */
+	long int tmp;
 
-  printf("%li, %li, ", x, y);
+	printf("%li, %li, ", x, y);
 
-  while (counter < 50)
-  {
-    printf("%li", x + y);
+	while (counter < 50)
+	{
+		printf("%li", x + y);
 
-    if (counter == 49)
-      break;
+		if (counter == 49)
+			break;
 
-    printf(", ");
-    tmp = x;
-    x = y;
-    y = x + tmp;
+		printf(", ");
+		tmp = x;
+		x = y;
+		y = x + tmp;
 
-    counter++;
-  }
-  putchar('\n');
+		counter++;
+	}
+	putchar('\n');
 
-  return (0);
+	return (0);
 }
